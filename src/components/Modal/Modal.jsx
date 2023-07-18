aea59d7b038c1603087dd3bc94a79478822be321
Kyrylo Lysenko <lysenko.ko@gmail.com>
2023-07-18 03:18:08 +0300

    Update Modal.jsx

--- src/components/Modal/Modal.jsx
+++ src/components/Modal/Modal.jsx
@@ -1,14 +1,45 @@
-// import PropTypes from 'prop-types';
-import css from './Modal.modal.css';
+import PropTypes from 'prop-types';
+import { Component } from 'react';
+import { createPortal } from 'react-dom';
+import css from './Modal.module.css';
 
-export const Modal = props => {
-  return (
-    <div className={css.overlay}>
-      <div className={css.modal}>
-        <img src="" alt="" />
-      </div>
-    </div>
-  );
+const modalRoot = document.querySelector('#modal-root');
+
+export class Modal extends Component {
+  //Вішаемо слухача на подію 'keydown' при монтуванні компонента модального вікна
+  componentDidMount() {
+    window.addEventListener('keydown', this.handleKeyDown);
+  }
+  //Видаляємо слухач на подію 'keydown' при розмонтуванні компонента модального вікна
+  componentWillUnmount() {
+    window.removeEventListener('keydown', this.handleKeyDown);
+  }
+
+  //Метод закриття модального вікна по Escape
+  handleKeyDown = e => {
+    if (e.code === 'Escape') {
+      this.props.onClose();
+    }
+  };
+
+  //Метод закриття модального по кліку на backdrop
+  handleBackdropClick = e => {
+    if (e.target === e.currentTarget) {
+      this.props.onClose();
+    }
+  };
+  //Компонент модального вікна буде рендеритись в окремий портал
+  render() {
+    return createPortal(
+      <div className={css.overlay} onClick={this.handleBackdropClick}>
+        <div className={css.modal}>{this.props.children}</div>
+      </div>,
+      modalRoot
+    );
+  }
+}
+
+Modal.propTypes = {
+  onCLose: PropTypes.func,
+  children: PropTypes.node.isRequired,
 };
-
-// Modal.propTypes = {};
