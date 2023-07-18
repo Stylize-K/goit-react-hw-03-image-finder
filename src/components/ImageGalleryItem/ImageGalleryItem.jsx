36ff214dd0cce4ba66b485c8b5e22ef96ad356c9
Kyrylo Lysenko <lysenko.ko@gmail.com>
2023-07-18 03:18:23 +0300

    Update ImageGalleryItem.jsx

--- src/components/ImageGalleryItem/ImageGalleryItem.jsx
+++ src/components/ImageGalleryItem/ImageGalleryItem.jsx
@@ -1,13 +1,14 @@
 import PropTypes from 'prop-types';
 import css from './ImageGalleryItem.module.css';
 
-export const ImageGalleryItem = ({ image }) => {
+export const ImageGalleryItem = ({ image, onClick }) => {
   return (
     <li className={css.imageGalleryItem}>
       <img
         className={css.imageGalleryItemImage}
         src={image.webformatURL}
         alt={image.tags}
+        onClick={() => onClick(image.largeImageURL, image.tags)}
       />
     </li>
   );
@@ -16,6 +17,8 @@ export const ImageGalleryItem = ({ image }) => {
 ImageGalleryItem.propTypes = {
   image: PropTypes.shape({
     webformatURL: PropTypes.string.isRequired,
+    largeImageURL: PropTypes.string.isRequired,
     tags: PropTypes.string.isRequired,
   }).isRequired,
+  onClick: PropTypes.func.isRequired,
 };
